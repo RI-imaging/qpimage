879a39af3214f0d8630726f976249dc02b781e44
paulmueller <paulmueller@users.noreply.github.com>
2017-09-29 14:47:14 +0200

    minor

--- examples/simple_phase.py
+++ examples/simple_phase.py
@@ -5,6 +5,7 @@ This example illustrates the simple usage of the
 quantitative phase data. The attribute ``QPImage.pha`` yields
 the background-corrected phase data and the attribute 
 ``QPImage.bg_pha`` yields the background phase image. 
+
 """
 import matplotlib.pylab as plt
 import numpy as np
