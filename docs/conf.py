fa701340819732964775f3d4912fa4ae1b0a6258
Paul Müller <dev@craban.de>
2021-04-12 22:56:12 +0200

    cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -13,15 +13,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-# Get version number from qpimage._version file
 import os
 import os.path as op
 import sys
