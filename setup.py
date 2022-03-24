a4f2b1a4db2966b96feef681fc1011bba0d39349
Paul Müller <dev@craban.de>
2022-03-25 00:56:23 +0100

    setup: bump qpretrieve from 0.2.0 to 0.2.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
                       "lmfit",
                       "nrefocus[FFTW]>=0.4.3",  # Refocus class
                       "numpy>=1.9.0",
-                      "qpretrieve[FFTW]>=0.2.0",
+                      "qpretrieve[FFTW]>=0.2.1",
                       "scikit-image>=0.11.0",
                       "scipy>=0.18.0",
                       ],
